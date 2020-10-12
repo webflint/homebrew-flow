277a004e1ab7afc154854a2cf4c5c20f88acf250
Brett Suwyn <brett.suwyn@sendoso.com>
2020-10-12 14:35:57 -0700

    Fix package URL

--- Formula/webflint-flow.rb
+++ Formula/webflint-flow.rb
@@ -2,7 +2,7 @@ require 'formula'
 class WebflintFlow < Formula
   homepage 'https://github.com/webflint/homebrew-flow'
   version '0.0.5'
-  url 'https://github.com/webflint/homebrew-flow/raw/master/dist/flow.tar.gz'
+  url 'https://github.com/webflint/homebrew-flow/raw/master/dist/flow-0.0.5.tar.gz'
   sha256 '9ea3fa59c02888d5e61a162daa8c4f47af23ebb91b6162e1a226f03ab9c5cd68'
   depends_on 'jq'
   def install
